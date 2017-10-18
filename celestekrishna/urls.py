e397a44308dc65973c65a60b1c4aad6f95c9a10d
Garrett Davis <garrett@imphatic.com>
2017-10-17 20:49:02 -0700

    working commit

--- celestekrishna/urls.py
+++ celestekrishna/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('home.urls')),
-    url(r'^preludered', include('preludered.urls')),
+    url(r'^preludered/', include('preludered.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- preludered/views.py
+++ preludered/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'preludered/index.html')
+
+
+def player(request):
+    return render(request, 'preludered/player.html')
