8b98ec0e5a99da46c9aecdc07779525b94f62fbf
Garrett Davis <garrett@imphatic.com>
2017-11-07 09:03:15 -0800

    started on admin

--- preludered/models.py
+++ preludered/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Music(models.Model):
+    track_number = models.IntegerField(default=0)
+    libretto_image = models.CharField(max_length=200)
+
+
+class Notes(models.Model):
+    image = models.CharField(max_length=200)
+    order = models.IntegerField(default=0)
+
+
+class Credits(models.Model):
+    image = models.CharField(max_length=200)
+    order = models.IntegerField(default=0)
+
+
+class MusicVideos(models.Model):
+    embed = models.CharField(max_length=500)
+    order = models.IntegerField(default=0)
